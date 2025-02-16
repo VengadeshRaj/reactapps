9a2d34a791347b0b3e86ac90bb82c8e9ef3d9143
vengadeshraj <vengadeshraj405@gmail.com>
2025-02-16 20:51:04 +0530

    app repo structure modified

---
+++ find-receipe/src/Route.tsx
@@ -0,0 +1,9 @@
+const Route = () => {
+  return (
+    <div>
+      <h1>Hello from route</h1>
+    </div>
+  );
+};
+
+export default Route;
---
+++ find-receipe/src/fr-components/PageLayout/PageLayout.tsx
@@ -0,0 +1,6 @@
+const PageLayout=({children}:any)=>
+    <div className="flex">
+        {children}
+    </div>
+
+export default PageLayout;
